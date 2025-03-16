41fdff10058a2060339d760a45908860af2a6618
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-16 12:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function findDOMLevel() {
+    let element = document.getElementById("level");
+    let level = 0;
+    
+    while (element) {
+        level++;
+        element = element.parentElement;
+    }
+    
+    alert(`The level of the element is: ${level}`);
+}
+
+findDOMLevel();
